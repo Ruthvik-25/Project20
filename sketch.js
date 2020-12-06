b079b4bea2c32b582b9ba7b08e6e3b752134c5bd
Ruthvik-25 <74710036+Ruthvik-25@users.noreply.github.com>
2020-12-06 09:36:37 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,39 +1,36 @@
+var car,wall;
+var speed,height;
 
+function setup(){
+    createCanvas(1600,400);
+speed=random(55,90)
+weight=random(400,1500)
 
-const Engine=Matter.Engine
-const World=Matter.World
-const Bodies=Matter.Bodies
-var engine,world,object
-function setup() {
-  createCanvas(1200,400);
-  engine=Engine.create()
-  world=engine.world
-  
-  box1=new Box(700,320,70,70)
-  box2=new Box(920,320,70,70)
-  ground=new Ground(600,height,width,20)
-  pig1=new Pig(810,350)
-  log1=new Log(810,260,300,PI/2)
-  box3=new Box(700,240,70,70)
-  box4=new Box(920,240,70,70)
-  pig2=new Pig(810,220)
-  log2=new Log(810,180,300,PI/2)
-  bird=new Bird(100,100)
-
-
+    car=createSprite(50,200,50,50);
+    car.velocityX=speed;
+    car.shapeColor=color(255);
+    
+    wall=createSprite(1500,200,60,height/2)
+    wall=shapeColor=color(80,80,80)
 }
-
-function draw() {
-  background("black"); 
-  Engine.update(engine) 
-  box1.display()
-  box2.display()
-  ground.display()
-  pig1.display()
-  log1.display()
-  box3.display()
-  box4.display()
-  pig2.display()
-  log2.display()
-  bird.display()
+function draw(){
+    background(0);
+    if(wall.x-car.x<(car.width+wall.width)/2)
+{
+  car.velocityX=0;
+  var deformation=0.5*weight*speed*speed/22509;
+  if(deformation>180)
+  {
+      car.shapeColor=color(255,0,0);
+  }
+  if (deformation<180 && deformation>100)
+  {
+    car.shapeColor=color(230,230,0);
+  }
+  if (deformation<100)
+  {
+      car.shapeColor=color(0,255,0);
+  }
+}
+    drawSprites();
 }
